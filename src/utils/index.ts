cb0cd662cbe4efcdf8a3472e9da26bc43556d479
soramon0 <23435312+soramon0@users.noreply.github.com>
2022-11-26 16:42:41 +0100

    feat: add lib methods and utils helpers

---
+++ src/utils/index.ts
@@ -0,0 +1,26 @@
+import { getAddress } from '@ethersproject/address';
+
+// returns the checksummed address if the address is valid, otherwise returns false
+export function isAddress(value: string): string | false {
+  try {
+    // Alphabetical letters must be made lowercase for getAddress to work.
+    // See documentation here: https://docs.ethers.io/v5/api/utils/address/
+    return getAddress(value.toLowerCase());
+  } catch {
+    return false;
+  }
+}
+
+export function shortenAddress(address: string, chars = 4): string {
+  const parsed = isAddress(address);
+
+  if (!parsed) {
+    throw Error(`Invalid 'address' parameter '${address}'.`);
+  }
+
+  return `${parsed.substring(0, chars + 2)}...${parsed.substring(42 - chars)}`;
+}
+
+export function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
