fb1fc9983acad4f9a7a531a83d9a8afa5ca14d41
soramon0 <23435312+soramon0@users.noreply.github.com>
2022-12-01 23:38:20 +0100

    feat: animate ZPKyc modal

--- src/components/Tokens/ZPKycModal.tsx
+++ src/components/Tokens/ZPKycModal.tsx
@@ -1,4 +1,5 @@
-import { Dialog } from '@headlessui/react';
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
 
 import { PaymentDetails } from '@/pages';
 
@@ -18,19 +19,40 @@ function ZPKycModal({
   paymentDetails,
 }: Props) {
   return (
-    <Dialog open={isOpen} onClose={close} className="relative z-50">
-      {/* The backdrop, rendered as a fixed sibling to the panel container */}
-      <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
+    <Transition show={isOpen} as={Fragment}>
+      <Dialog onClose={close}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          {/* The backdrop, rendered as a fixed sibling to the panel container */}
+          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+        </Transition.Child>
 
-      <div className="fixed inset-0 flex items-center justify-center p-4">
-        <Dialog.Panel className="w-full max-w-sm rounded-xl bg-white p-10">
-          <Dialog.Title className="text-center">ZPKyc</Dialog.Title>
-          <Dialog.Description className="text-center">
-            Validating your wallet address...
-          </Dialog.Description>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+        <Transition.Child
+          as="div"
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+          className="fixed inset-0 flex items-center justify-center p-4"
+        >
+          <Dialog.Panel className="w-full max-w-sm rounded-xl bg-white p-10">
+            <Dialog.Title className="text-center">ZPKyc</Dialog.Title>
+            <Dialog.Description className="text-center">
+              Validating your wallet address...
+            </Dialog.Description>
+          </Dialog.Panel>
+        </Transition.Child>
+      </Dialog>
+    </Transition>
   );
 }
 
