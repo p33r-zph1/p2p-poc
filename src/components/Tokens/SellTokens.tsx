8ae4796924f0454a9adacbec116ca3abc07c7a65
soramon0 <23435312+soramon0@users.noreply.github.com>
2022-12-02 20:48:30 +0100

    feat: add finding pair section

--- src/components/Tokens/SellTokens.tsx
+++ src/components/Tokens/SellTokens.tsx
@@ -3,11 +3,12 @@ import { XMarkIcon, MinusIcon } from '@heroicons/react/20/solid';
 import { useDebounce } from 'use-debounce';
 
 import { PaymentDetails } from '@/pages';
-import { onlyNumbers } from '@/utils';
+import { classNames, onlyNumbers } from '@/utils';
 import useTokenTransfer from '@/hooks/useTokenTransfer';
 
 import CurrencySelector from './CurrencySelector';
 import { InlineErrorDisplay } from '../shared';
+import { MatchedIcon, MatchingIcon } from '../icons';
 
 const tokens = [
   { name: 'ETH', icon: '/images/ethereum.svg' },
@@ -26,10 +27,14 @@ interface Props {
   connectWallet(): void;
 }
 
+type FindingPairStatus = 'idle' | 'findingPair' | 'pairFound' | 'pairNotFound';
+
 function SellTokens({ paymentDetails, connected, connectWallet }: Props) {
   const [error, setError] = useState('');
   const [tokenAmount, setTokenAmount] = useState('');
   const [debouncedTokenAmount] = useDebounce(tokenAmount, 100);
+  const [findingPairStatus, setFindingPairStatus] =
+    useState<FindingPairStatus>('idle');
 
   const [fiatAmount, setFiatAmount] = useState('');
   const [debouncedFiatAmount] = useDebounce(fiatAmount, 100);
@@ -48,6 +53,20 @@ function SellTokens({ paymentDetails, connected, connectWallet }: Props) {
       setError('Provide your payment details to transact.');
       return;
     }
+  };
+
+  const findPair = () => {
+    setFindingPairStatus('findingPair');
+
+    setTimeout(() => {
+      setFindingPairStatus('pairFound');
+    }, 2000);
+  };
+
+  const transferFunds = () => {
+    if (!transfer) return;
+
+    transfer();
   };
 
   const tokenAmountHandler = useCallback(
@@ -142,12 +161,65 @@ function SellTokens({ paymentDetails, connected, connectWallet }: Props) {
 
       <InlineErrorDisplay show={Boolean(error)} error={error} />
 
-      {connected && (
+      {connected && findingPairStatus !== 'idle' && (
+        <div className="flex flex-col items-center justify-center">
+          {(findingPairStatus === 'findingPair' ||
+            findingPairStatus === 'pairNotFound') && (
+            <MatchingIcon className="text-white" />
+          )}
+
+          {findingPairStatus === 'pairFound' && (
+            <MatchedIcon className="text-white" />
+          )}
+
+          <p className="mt-4 text-xl font-semibold">
+            {findingPairStatus === 'findingPair' && 'Matching you with a buyer'}
+            {findingPairStatus === 'pairFound' && 'Buyer found!'}
+            {findingPairStatus === 'pairNotFound' && 'Could not find Buyer.'}
+          </p>
+          <p
+            className={classNames(
+              findingPairStatus === 'findingPair' ? 'animate-pulse' : '',
+              'text-sm text-sleep-100'
+            )}
+          >
+            {findingPairStatus === 'findingPair' &&
+              ' This may take a few minutes...'}
+            {findingPairStatus === 'pairFound' &&
+              'Send crypto to our secure escrow account'}
+            {findingPairStatus === 'pairNotFound' && 'Please try again later.'}
+          </p>
+        </div>
+      )}
+
+      {connected && findingPairStatus === 'pairFound' && (
+        <button
+          type="submit"
+          onClick={transferFunds}
+          className="w-full rounded-4xl bg-brand px-4 py-3 text-sm font-bold text-white hover:bg-brand/90 focus:outline-none focus:ring focus:ring-brand/80 active:bg-brand/80 disabled:bg-sleep disabled:text-sleep-300"
+        >
+          Send Crypto
+        </button>
+      )}
+
+      {connected &&
+        findingPairStatus !== 'idle' &&
+        findingPairStatus !== 'pairFound' && (
+          <button
+            type="submit"
+            disabled={findingPairStatus === 'findingPair'}
+            className="w-full rounded-4xl bg-brand px-4 py-3 text-sm font-bold text-white hover:bg-brand/90 focus:outline-none focus:ring focus:ring-brand/80 active:bg-brand/80 disabled:bg-sleep disabled:text-sleep-300"
+          >
+            Processing
+          </button>
+        )}
+
+      {connected && findingPairStatus === 'idle' && (
         <button
           type="submit"
           disabled={!Boolean(paymentDetails)}
           className="w-full rounded-4xl bg-brand px-4 py-3 text-sm font-bold text-white hover:bg-brand/90 focus:outline-none focus:ring focus:ring-brand/80 active:bg-brand/80 disabled:bg-sleep disabled:text-sleep-300"
-          onClick={() => transfer?.()}
+          onClick={findPair}
         >
           Confirm
         </button>
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,6 @@
 export { default as BankIcon } from './BankIcon';
 export { default as Logo } from './Logo';
 export { default as LogoIcon } from './LogoIcon';
+export { default as MatchingIcon } from './MatchingIcon';
+export { default as MatchedIcon } from './MatchedIcon';
 export { default as WalletIcon } from './WalletIcon';
