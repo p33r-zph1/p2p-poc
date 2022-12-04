d09898bed0d6b380eb4f104df2a7d43dd1499fb3
soramon0 <23435312+soramon0@users.noreply.github.com>
2022-12-04 21:25:10 +0100

    feat: hide transactions page if wallet is not connected

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -49,21 +49,42 @@ function TransactionsPage() {
       <main className="flex-1 p-4 md:flex md:items-center md:justify-center md:p-8 lg:px-10">
         <div className="mt-16 w-full md:mt-0">
           <div className="md:px-5 lg:px-10">
-            <h1 className="text-xl font-semibold text-body md:text-2xl">
-              Transactions
-            </h1>
+            {isConnected ? (
+              <>
+                <h1 className="text-xl font-semibold text-body md:text-2xl">
+                  Transactions
+                </h1>
 
-            <div className="my-3">
-              {isFetching ? (
-                <div className="h-2.5 w-12 animate-pulse rounded-full bg-gray-300"></div>
-              ) : (
-                <p className="text-sm text-sleep-100">{data?.length} results</p>
-              )}
-            </div>
+                <div className="my-3">
+                  {isFetching ? (
+                    <div className="h-2.5 w-12 animate-pulse rounded-full bg-gray-300"></div>
+                  ) : (
+                    <p className="text-sm text-sleep-100">
+                      {data?.length} results
+                    </p>
+                  )}
+                </div>
 
-            <div className="my-5">
-              <Transactions />
-            </div>
+                <div className="my-5">
+                  <Transactions />
+                </div>
+              </>
+            ) : (
+              <div className="mx-auto max-w-md rounded-xl bg-white p-5">
+                <h3 className="m-5 text-center font-sans text-xl font-semibold md:text-2xl">
+                  Please connect your wallet
+                </h3>
+
+                <button
+                  type="button"
+                  className="w-full rounded-4xl bg-brand px-4 py-3 text-sm font-bold text-white hover:bg-brand/90 focus:outline-none focus:ring focus:ring-brand/80 active:bg-brand/80 disabled:bg-sleep disabled:text-sleep-300"
+                  disabled={isConnecting}
+                  onClick={() => connect()}
+                >
+                  {isConnecting ? 'Connecting...' : 'Connect Wallet'}
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </main>
